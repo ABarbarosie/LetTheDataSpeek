a614ac8ca5cf58387043e49012591b4f7b434dce
ABarbarosie <a.barbarosie@jacobs-university.de>
2014-03-12 21:49:19 +0100

    Added WebPage class

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
-import Tkinter
-top = Tkinter.Tk()
-# Code to add widgets will go here...
-top.mainloop()
+from Tkinter import Tk, Label
+from ttk import Frame, Style
+from webpage import WebPage
+
+class Application(Frame):
+  
+    def __init__(self, parent):
+        Frame.__init__(self, parent)   
+         
+        self.parent = parent
+        
+        self.initUI()
+        
+    def initUI(self):
+        self.parent.title("Let the Data Speek")
+        page = WebPage(["The","words","go","here"])
+        print page.link + "\n"
+        page.getContent()
+        print page.content
+               
+              
+
+def main():
+    root = Tk()
+    root.geometry("300x280+300+300")
+    app = Application(root)
+    root.mainloop()  
+
+if __name__ == '__main__':
+    main()  
