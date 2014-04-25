7b4a6faf1fc52d6a2dbf348f200f8dc8c0e5736a
Kiril Kafadarov <k.kafadarov@jacobs-university.de>
2014-04-25 11:53:01 +0300

    CSV data fetcher beta 0.1 Data is successfully downloaded if user is logged in on google. TODO: automatically login before fetching For now you can work on the parsing using it the way it is.

--- webpage.py
+++ webpage.py
@@ -2,20 +2,21 @@ import urllib
 import urllib2
 
 class WebPage:
-	def __init__(self, words):
-		base = "http://www.google.com/trends/explore#q="
-		terminate = "&cmpt=q"
-		separator = "%2C"
-		if len(words) == 1:
-			self.link = base + words[0] + terminate
-		elif len(words) > 1:
-			self.link = base
-			for i in range(0,len(words)-1):
-				self.link += words[i] + separator
-			self.link += words[len(words)-1] + terminate 
-		else:
-			self.link = ""
+  def __init__(self, words):
+    base = "http://www.google.com/trends/viz?q="
+    terminate = "&date=all&geo=all&graph=all_csv&sort=0&sa=N"
+    separator = ",+"
+    
+    if len(words) == 1:
+      self.link = base + words[0] + terminate
+    elif len(words) > 1:
+      self.link = base
+      for i in range(0,len(words)-1):
+        self.link += words[i] + separator
+      self.link += words[len(words)-1] + terminate 
+    else:
+      self.link = ""
 
-	def getContent(self):
-		intermediate = urllib2.urlopen(self.link)
-		self.content = intermediate.read()
+  def getContent(self):
+    intermediate = urllib2.urlopen(self.link)
+    self.content = intermediate.read()
